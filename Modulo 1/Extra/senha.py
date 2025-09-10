da808ec39d117e4ae5c985ee54249e555d2e1412
murilokm12 <murilosantos2405@gmail.com>
2025-09-10 16:43:30 -0300

    Add files via upload

---
+++ Modulo 1/Extra/senha.py
@@ -0,0 +1,22 @@
+nome = input("digite seu nome de usuário: ")
+
+senha = input("digite sua senha: ")
+
+def nomes (senha):
+
+    if senha== "Murilokm122405":
+
+        print(f'acesso liberado')
+    
+ 
+
+def numeros (nome):
+
+    if nome == "123456":
+        print(f'acesso liberado')
+    
+    else :
+        print(f'Nome de Usuário Incorreto e senha Incorreta!')
+
+numeros(senha)
+nomes(nome)
