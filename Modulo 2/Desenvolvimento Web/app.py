4b3cf50f27a8a3e9452335b2e2d23d32495aa9ce
murilokm12 <murilosantos2405@gmail.com>
2025-09-11 17:39:40 -0300

    Add files via upload

---
+++ Modulo 2/Desenvolvimento Web/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+import streamlit as st
+
+# Titulo
+st.title("Lista de contatos")
+
+st.write("Meus contatos")
+
+
+contatos=[
+    {"nome":"roberto","email":"robertogamepalys@gmail","telefone":"11221312343"},
+    {"nome":"roberto","email":"robertogamepalys@gmail","telefone":"11221312343"},
+    {"nome":"roberto","email":"robertogamepalys@gmail","telefone":"11221312343"},
+    {"nome":"roberto","email":"robertogamepalys@gmail","telefone":"11221312343"}
+
+
+
+
+
+]
+
+
+
+st.table(contatos)
